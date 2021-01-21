e2a9bc7de85f293f9eddd80a2dadc05e345d05c1
Gamecheat13 <gamecheat@blamdepot.com>
2021-01-21 03:08:18 -0500

    Halo 5 ACTUAL 77316.15.12.10.1718-3

---
+++ Halo 5 Guardians/77316.15.12.10.1718-3/tags/multiplayer/lua/vehicleconstants.lua
@@ -0,0 +1,40 @@
+
+BansheeType = CampaignMetagameType.Banshee;
+GhostType = CampaignMetagameType.Ghost;
+MantisType = CampaignMetagameType.Mantis;
+MongooseType = CampaignMetagameType.Mongoose;
+WaspType = CampaignMetagameType.Wasp;
+PhaetonType = CampaignMetagameType.Phaeton;
+ScorpionType = CampaignMetagameType.Scorpion;
+WarthogType = CampaignMetagameType.Warthog;
+WraithType = CampaignMetagameType.Wraith;
+PhantomType = CampaignMetagameType.Phantom;
+AllVehicleTypes = 
+{
+	BansheeType,
+	GhostType,
+	MantisType,
+	MongooseType,
+	WaspType,
+	PhaetonType,
+	ScorpionType,
+	WarthogType,
+	WraithType,
+	PhantomType
+};
+LandVehicleTypes = 
+{
+	GhostType,
+	MantisType,
+	MongooseType,
+	ScorpionType,
+	WarthogType,
+	WraithType
+};
+AirVehicleTypes = 
+{
+	BansheeType,
+	WaspType,
+	PhaetonType,
+	PhantomType
+};
